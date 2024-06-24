830b16205c01733283553a4037249c402e773a7a
Huy Phan <huy.phan2210@outlook.com>
2024-06-24 11:43:31 +0700

    Update app component

--- multi-step_form.client/src/app/app.component.ts
+++ multi-step_form.client/src/app/app.component.ts
@@ -11,7 +11,7 @@ interface WeatherForecast {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   public forecasts: WeatherForecast[] = [];
