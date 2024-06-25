b6fb9b8c2795f46e86c3030c265fd52990ef1941
Huy Phan <huy.phan2210@outlook.com>
2024-06-25 13:20:49 +0700

    Update app.component.ts

--- multi-step_form.client/src/app/app.component.ts
+++ multi-step_form.client/src/app/app.component.ts
@@ -1,12 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
-}
 
 @Component({
   selector: 'app-root',
@@ -14,24 +6,7 @@ interface WeatherForecast {
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  public forecasts: WeatherForecast[] = [];
-
-  constructor(private http: HttpClient) {}
-
   ngOnInit() {
-    this.getForecasts();
+    // this.getForecasts();
   }
-
-  getForecasts() {
-    this.http.get<WeatherForecast[]>('/weatherforecast').subscribe(
-      (result) => {
-        this.forecasts = result;
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
-  }
-
-  title = 'multi-step_form.client';
 }
