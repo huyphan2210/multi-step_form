a0de88a50b64bc047543ad99fbc0f908ce44eae2
Huy Phan <huy.phan2210@outlook.com>
2024-06-26 13:21:28 +0700

    Update DTOs

--- multi-step_form.Server/Models/DTOs/PersonalInfoRequest.cs
+++ multi-step_form.Server/Models/DTOs/PersonalInfoRequest.cs
@@ -1,8 +1,24 @@
 ﻿namespace multi_step_form.Server.Models.DTOs
 {
-    public class PersonalInfoRequest : PersonalInfo
+    public class PersonalInfoRequest
     {
-        public List<Guid>? AddOnIds { get; set; }
-        public Guid PlanId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public List<string>? AddOnIds { get; set; }
+        public string PlanId { get; set; } = string.Empty;
+
+        public PersonalInfo ParseRequestToPersonalInfo()
+        {
+            return new PersonalInfo
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = Name,
+                Email = Email,
+                Phone = Phone,
+                AddOnIds = AddOnIds,
+                PlanId = PlanId
+            };
+        }
     }
 }
--- multi-step_form.Server/Models/DTOs/PersonalInfoResponse.cs
+++ multi-step_form.Server/Models/DTOs/PersonalInfoResponse.cs
@@ -1,8 +1,11 @@
 ﻿namespace multi_step_form.Server.Models.DTOs
 {
-    public class PersonalInfoResponse : PersonalInfo
+    public class PersonalInfoResponse
     {
-        public List<AddOn>? AddOns { get; set; }
-        public Plan? Plan { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string? PlanId { get; set; }
+        public List<string>? AddOnIds { get; set; }
     }
 }
