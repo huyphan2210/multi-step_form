3861078df5245f0e57d656b7d3eefe198e430f6d
Huy Phan <huy.phan2210@outlook.com>
2024-06-26 13:24:40 +0700

    Register new services

--- multi-step_form.Server/Program.cs
+++ multi-step_form.Server/Program.cs
@@ -4,7 +4,9 @@ using Microsoft.OpenApi.Extensions;
 using multi_step_form.Server.DataAccess.AddOnCollection;
 using multi_step_form.Server.DataAccess.PersonalInfoCollection;
 using multi_step_form.Server.DataAccess.PlanCollection;
-using multi_step_form.Server.Services;
+using multi_step_form.Server.Services.AddOnService;
+using multi_step_form.Server.Services.PersonalInfoService;
+using multi_step_form.Server.Services.PlanService;
 using Swashbuckle.AspNetCore.Swagger;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,6 +16,8 @@ builder.Services.AddControllers();
 
 // Add services to the container.
 builder.Services.AddScoped<IPersonalInfoService, PersonalInfoService>();
+builder.Services.AddScoped<IPlanService, PlanService>();
+builder.Services.AddScoped<IAddOnService, AddOnService>();
 
 //Add collections to the container
 builder.Services.AddScoped<IPersonalInfoCollection, PersonalInfoCollection>();
