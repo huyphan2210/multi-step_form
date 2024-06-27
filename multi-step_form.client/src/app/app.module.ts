82fa6664679ffeea31a111d4cf79ddda7c76ec8f
Huy Phan <huy.phan2210@outlook.com>
2024-06-27 15:07:43 +0700

    Import components

--- multi-step_form.client/src/app/app.module.ts
+++ multi-step_form.client/src/app/app.module.ts
@@ -3,9 +3,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormComponent } from 'src/components/Form/form.component';
+import { StepBarComponent } from 'src/components/StepBar/step-bar.component';
+import { StepperComponent } from 'src/components/Stepper/stepper.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    StepBarComponent,
+    FormComponent,
+    StepperComponent,
+  ],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
