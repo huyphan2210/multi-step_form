6d049fd65d2780631a2f22ee82d48405003f306d
Huy Phan <huy.phan2210@outlook.com>
2024-06-27 15:07:28 +0700

    Create components

---
+++ multi-step_form.client/src/components/Form/form.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'form-component',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss'],
+})
+export class FormComponent implements OnInit {
+  ngOnInit() {
+    // this.getForecasts();
+  }
+}
---
+++ multi-step_form.client/src/components/StepBar/step-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'step-bar',
+  templateUrl: './step-bar.component.html',
+  styleUrls: ['./step-bar.component.scss'],
+})
+export class StepBarComponent implements OnInit {
+  ngOnInit() {
+    // this.getForecasts();
+  }
+}
