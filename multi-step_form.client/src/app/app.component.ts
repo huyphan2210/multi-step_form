a4dab1507513a51e408ba88bbd6bd5208610ac3b
Huy Phan <huy.phan2210@outlook.com>
2024-06-28 13:18:23 +0700

    Remove constructor

--- multi-step_form.client/src/app/app.component.ts
+++ multi-step_form.client/src/app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import axios from 'axios';
 import AddOnAPI from 'src/api/add-on.api';
-import { environment } from 'src/environments/environment';
-import { serviceOptions } from 'src/swagger/api';
 
 @Component({
   selector: 'app-root',
@@ -10,15 +7,7 @@ import { serviceOptions } from 'src/swagger/api';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  constructor(private addOnAPI: AddOnAPI) {
-    serviceOptions.axios = axios.create({
-      baseURL: environment.baseUrl,
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-    });
-  }
+  constructor(private addOnAPI: AddOnAPI) {}
 
   ngOnInit() {
     this.addOnAPI.getAddOns();
