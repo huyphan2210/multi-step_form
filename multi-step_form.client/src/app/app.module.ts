f701770c040b59e90e58719ef16cbf62077195e9
Huy Phan <huy.phan2210@outlook.com>
2024-06-28 13:19:16 +0700

    Update app.module.ts

--- multi-step_form.client/src/app/app.module.ts
+++ multi-step_form.client/src/app/app.module.ts
@@ -1,21 +1,30 @@
+import axios from 'axios';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { serviceOptions } from 'src/swagger/api';
 
+import { environment } from 'src/environments/environment';
 import { AppComponent } from './app.component';
 import { FormComponent } from 'src/components/Form/form.component';
-import { StepBarComponent } from 'src/components/StepBar/step-bar.component';
 import { StepperComponent } from 'src/components/Stepper/stepper.component';
+import { StepBarModule } from 'src/components/StepBar/step-bar.module';
+import { StepStateService } from 'src/state-service/step-state.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    StepBarComponent,
-    FormComponent,
-    StepperComponent,
-  ],
-  imports: [BrowserModule, HttpClientModule],
-  providers: [],
+  declarations: [AppComponent, FormComponent, StepperComponent],
+  imports: [BrowserModule, HttpClientModule, StepBarModule],
+  providers: [StepStateService],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    serviceOptions.axios = axios.create({
+      baseURL: environment.baseUrl,
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+}
