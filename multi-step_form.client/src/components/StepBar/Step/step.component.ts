cea1efd5bdec38b2ab409bc52f67ffef6e28ee81
Huy Phan <huy.phan2210@outlook.com>
2024-07-01 13:41:44 +0700

    Update step + stepbar components

--- multi-step_form.client/src/components/StepBar/Step/step.component.ts
+++ multi-step_form.client/src/components/StepBar/Step/step.component.ts
@@ -1,12 +1,27 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  Renderer2,
+  ElementRef,
+  ViewChild,
+  AfterViewInit,
+} from '@angular/core';
 
 @Component({
   selector: 'step',
   templateUrl: './step.component.html',
   styleUrls: ['./step.component.scss'],
 })
-export class StepComponent {
+export class StepComponent implements AfterViewInit {
   @Input() stepCount: number = 0;
   @Input() stepName: string = '';
   @Input() isCurrentStep: boolean = false;
+  @ViewChild('stepCountRef') stepCountRef?: ElementRef<HTMLDivElement>;
+
+  ngAfterViewInit(): void {
+    if (this.isCurrentStep && this.stepCountRef) {
+      this.stepCountRef.nativeElement.className += ' current';
+    }
+  }
 }
