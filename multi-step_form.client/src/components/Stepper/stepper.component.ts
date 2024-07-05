07995a8fa66f91f2128e664db6654e0b1368c55c
Huy Phan <huy.phan2210@outlook.com>
2024-07-05 14:14:46 +0700

    Subscibe to formState

--- multi-step_form.client/src/components/Stepper/stepper.component.ts
+++ multi-step_form.client/src/components/Stepper/stepper.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import {
+  FormStateService,
+  PersonalInfoFormControls,
+  personalInfoKeys,
+} from 'src/state-service/form-state.service';
 import { Step, StepStateService } from 'src/state-service/step-state.service';
 
 @Component({
@@ -7,13 +13,48 @@ import { Step, StepStateService } from 'src/state-service/step-state.service';
   styleUrls: ['./stepper.component.scss'],
 })
 export class StepperComponent implements OnInit {
-  constructor(private stepStateService: StepStateService) {}
+  constructor(
+    private stepStateService: StepStateService,
+    private formStateService: FormStateService
+  ) {}
 
   currentStep: Step = Step.FillInPersonalInfo;
   step = Step;
+  isNextButtonDisabled = false;
+  form!: FormGroup<PersonalInfoFormControls>;
+
   ngOnInit() {
     this.stepStateService.getStepState().subscribe((stepState) => {
       this.currentStep = stepState.step;
     });
+
+    this.formStateService
+      .getFormStateState()
+      .subscribe((formState: FormGroup<PersonalInfoFormControls>) => {
+        this.form = formState;
+      });
   }
+
+  previousStep() {
+    this.stepStateService.setPreviousStepState();
+  }
+
+  nextStep() {
+    this.stepStateService.setNextStepState();
+  }
+
+  checkIfNextButtonIsDisabled = () => {
+    const isNameValid = this.form.get(personalInfoKeys.name)?.valid;
+    const isEmailValid = this.form.get(personalInfoKeys.email)?.valid;
+    const isPhoneValid = this.form.get(personalInfoKeys.phone)?.valid;
+    const isPlanValid = this.form.get(personalInfoKeys.planId)?.valid;
+    switch (this.currentStep) {
+      case Step.FillInPersonalInfo:
+        return !isNameValid || !isEmailValid || !isPhoneValid;
+      case Step.SelectPlan:
+        return !isPlanValid;
+      default:
+        return false;
+    }
+  };
 }
