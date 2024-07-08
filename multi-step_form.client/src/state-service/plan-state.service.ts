564ed8152e0e6eb32decc3f9e11d212e403d1d68
Huy Phan <huy.phan2210@outlook.com>
2024-07-08 12:13:37 +0700

    Create plan-state.service.ts

---
+++ multi-step_form.client/src/state-service/plan-state.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import PlanAPI from 'src/api/plan.api';
+import { Plan } from 'src/swagger/api';
+
+export type PriceType = 'month' | 'year';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PlanStateService {
+  private plansState$: BehaviorSubject<Plan[]> = new BehaviorSubject<Plan[]>(
+    []
+  );
+  private priceTypeState$: BehaviorSubject<PriceType> =
+    new BehaviorSubject<PriceType>('month');
+
+  constructor(private planAPI: PlanAPI) {}
+
+  getPlanState() {
+    return this.plansState$.asObservable();
+  }
+
+  getPriceTypeState() {
+    return this.priceTypeState$.asObservable();
+  }
+
+  getPlans() {
+    this.planAPI.getPlans().then((plans) => {
+      this.plansState$.next(plans);
+    });
+  }
+
+  changePriceType() {
+    const currentPriceType = this.priceTypeState$.getValue();
+    if (currentPriceType === 'month') {
+      this.priceTypeState$.next('year');
+    } else {
+      this.priceTypeState$.next('month');
+    }
+  }
+}
