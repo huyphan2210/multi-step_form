3baaf8adba3a647015f41e4017c92a301c54681f
Huy Phan <huy.phan2210@outlook.com>
2024-07-09 15:06:57 +0700

    Add & Use ResizeObserverDirective

--- multi-step_form.client/src/app/app.component.ts
+++ multi-step_form.client/src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component, ComponentFactoryResolver, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { Step, StepStateService } from 'src/state-service/step-state.service';
 
 @Component({
@@ -7,15 +14,33 @@ import { Step, StepStateService } from 'src/state-service/step-state.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  constructor(private stepStateService: StepStateService) {}
+  constructor(
+    private stepStateService: StepStateService,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  @ViewChild('formWrapper') formWrapperRef!: ElementRef<HTMLDivElement>;
 
   isStepperHasOneButton = false;
   isStepperShown = false;
+  isStepperSticky = false;
 
   ngOnInit() {
     this.stepStateService.getStepState().subscribe((stepState) => {
       this.isStepperHasOneButton = stepState.step === Step.FillInPersonalInfo;
       this.isStepperShown = stepState.step !== Step.ThankYou;
     });
+  }
+
+  handleFormSizeChange(formDom: DOMRectReadOnly) {
+    if (
+      formDom.height >
+      window.innerHeight - (window.innerHeight * 25) / 100 - 111
+    ) {
+      this.isStepperSticky = true;
+    } else {
+      this.isStepperSticky = false;
+    }
+    this.cdr.detectChanges();
   }
 }
--- multi-step_form.client/src/app/app.module.ts
+++ multi-step_form.client/src/app/app.module.ts
@@ -3,16 +3,17 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { serviceOptions } from 'src/swagger/api';
-
 import { environment } from 'src/environments/environment';
+
 import { AppComponent } from './app.component';
 import { StepperComponent } from 'src/components/Stepper/stepper.component';
 import { StepBarModule } from 'src/components/StepBar/step-bar.module';
 import { StepStateService } from 'src/state-service/step-state.service';
 import { FormModule } from 'src/components/Form/form.module';
+import { ResizeObserverDirective } from 'src/directives/resize-observer.directive';
 
 @NgModule({
-  declarations: [AppComponent, StepperComponent],
+  declarations: [AppComponent, StepperComponent, ResizeObserverDirective],
   imports: [BrowserModule, HttpClientModule, StepBarModule, FormModule],
   providers: [StepStateService],
   bootstrap: [AppComponent],
