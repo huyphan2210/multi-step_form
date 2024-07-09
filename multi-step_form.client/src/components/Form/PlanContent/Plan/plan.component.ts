b3181dcf68e972251f6c90f74b18142f6d2bdd78
Huy Phan <huy.phan2210@outlook.com>
2024-07-09 15:07:20 +0700

    Update plan

--- multi-step_form.client/src/components/Form/PlanContent/Plan/plan.component.ts
+++ multi-step_form.client/src/components/Form/PlanContent/Plan/plan.component.ts
@@ -1,7 +1,9 @@
 import { Component, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-import { PersonalInfoFormControls } from 'src/state-service/form-state.service';
-import { PriceType } from 'src/state-service/plan-state.service';
+import {
+  PersonalInfoFormControls,
+  PriceType,
+} from 'src/state-service/form-state.service';
 import { Plan } from 'src/swagger/api';
 
 @Component({
@@ -13,8 +15,5 @@ export class PlanComponent {
   @Input() form!: FormGroup<PersonalInfoFormControls>;
   @Input() plan!: Plan;
   @Input() priceType: PriceType = 'month';
-
-  setPlan() {
-    console.log(this.form.get('planId')?.value);
-  }
+  @Input() isCurrentPlan: boolean = false;
 }
