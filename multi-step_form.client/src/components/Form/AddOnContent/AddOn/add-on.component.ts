4fc93278182967392b019ee70434a66303a66f9f
Huy Phan <huy.phan2210@outlook.com>
2024-07-22 11:41:50 +0700

    Change handleAddOn

--- multi-step_form.client/src/components/Form/AddOnContent/AddOn/add-on.component.ts
+++ multi-step_form.client/src/components/Form/AddOnContent/AddOn/add-on.component.ts
@@ -11,28 +11,24 @@ import { AddOn, PriceType } from 'src/swagger/api';
   templateUrl: './add-on.component.html',
   styleUrls: ['./add-on.component.scss'],
 })
-export class AddOnComponent implements OnInit {
+export class AddOnComponent {
   @Input() form!: FormGroup<PersonalInfoFormControls>;
   @Input() addOn!: AddOn;
   @Input() isActive: boolean = false;
+  @Input() currentPriceType!: PriceType;
 
-  currentPriceType: PriceType = 0;
-
-  ngOnInit(): void {
-    this.currentPriceType = this.form.get('currentPriceType')?.value ?? 0;
-  }
-
-  handleAddOn(e: Event) {
+  handleAddOn(e: MouseEvent) {
     const checkBox = e.currentTarget as HTMLInputElement;
     const addOnControl = this.form.get(personalInfoKeys.addOnIds);
-    if (checkBox.checked && addOnControl?.value) {
+    const addOnIds = addOnControl?.value as string[];
+    const index = addOnIds.indexOf(this.addOn.id!);
+
+    if (addOnControl?.value && index === -1) {
       addOnControl?.patchValue([...addOnControl.value, this.addOn.id]);
-    } else if (!checkBox.checked && addOnControl?.value) {
-      const addOnIds = addOnControl.value as string[];
-      const index = addOnIds.indexOf(this.addOn.id!);
-      if (index > -1) {
-        addOnIds.slice(index);
-      }
+    } else if (addOnControl?.value && index > -1) {
+      addOnIds.splice(index, 1);
+      addOnControl?.patchValue(addOnIds);
+      checkBox.checked = false;
     }
   }
 }
