d2e7cb7a89b596ca5cabecf04087da97d0f6e954
Huy Phan <huy.phan2210@outlook.com>
2024-07-24 17:00:45 +0700

    Update PlanCollection.cs

--- multi-step_form.Server/DataAccess/PlanCollection/PlanCollection.cs
+++ multi-step_form.Server/DataAccess/PlanCollection/PlanCollection.cs
@@ -6,13 +6,15 @@ namespace multi_step_form.Server.DataAccess.PlanCollection
     public class PlanCollection : IPlanCollection
     {
         private readonly CollectionReference _planReference;
-
-        public PlanCollection()
+        private readonly ILogger _logger;
+        public PlanCollection(ILogger logger)
         {
             // SET GOOGLE_APPLICATION_CREDENTIALS in the EnvironmentVariable of your local machine/hosting service
             // var path = "Path to your firebase-adminsdk.json file"; // e.g., "firebase-adminsdk.json"
             // Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", path);
+            _logger = logger;
             var projectId = Environment.GetEnvironmentVariable("FIRESTORE_FORM_PROJECT-ID");
+            _logger.LogInformation($"project {projectId}");
             var fireStoreDb = FirestoreDb.Create(projectId);
             _planReference = fireStoreDb.Collection(nameof(Plan));
         }
