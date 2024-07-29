455be651ff8669b98618156af46aaae68065424d
Huy Phan <huy.phan2210@outlook.com>
2024-07-29 13:26:12 +0700

    Update Program.cs

--- multi-step_form.Server/Program.cs
+++ multi-step_form.Server/Program.cs
@@ -28,7 +28,7 @@ builder.Services.AddScoped<IAddOnCollection, AddOnCollection>();
 
 //Add FireStoreDb to the Container
 var json = Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS");
-var googleCredential = GoogleCredential.FromJson(json);
+var googleCredential = GoogleCredential.FromFile(json);
 
 var fireStoreBuilder = new FirestoreDbBuilder
 {
